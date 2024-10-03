e1bbb82d77214518efcf29a4511de109c5358ac1
Daniel M. Matongo <mmatongo_@hotmail.com>
2024-10-03 18:02:28 +0100

    add some basic utils

---
+++ utils/utils.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"mime"
+	"path/filepath"
+	"strings"
+)
+
+func CleanPath(path string) string {
+	path = strings.Split(path, "?")[0]
+	path = strings.ReplaceAll(path, "\\", "/")
+	return strings.ReplaceAll(path, "//", "/")
+}
+
+func GetMimeType(filePath string) string {
+	ext := filepath.Ext(filePath)
+	mimeType := mime.TypeByExtension(ext)
+	if mimeType == "" {
+		mimeType = "application/octet-stream"
+	}
+	return mimeType
+}
